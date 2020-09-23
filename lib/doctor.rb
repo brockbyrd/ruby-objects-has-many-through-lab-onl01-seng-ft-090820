268a7a52f99a6fbdde369ad00d63f876ce56fdca
Brock Byrd <brockbyrdd@gmail.com>
2020-09-23 19:04:04 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -9,8 +9,8 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select do |a|
-      a.doctor == self
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
     end
   end
 
